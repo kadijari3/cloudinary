c961bf8a38177028128dddf918b2b7645a144399
bocil sesat <kadijari3@gmail.com>
2025-10-06 15:55:13 +0700

    Create placing.js

---
+++ placing.js
@@ -0,0 +1,11 @@
+		jQuery(document).ready(function() {
+			var elumonyet = document.querySelector('.row.g-1.my-3.d-xl-none');
+		jQuery("#gacor").insertAfter(elumonyet);
+		
+		});
+
+		jQuery(document).ready(function() {
+			var jelong = document.getElementById('#row-togel');
+		jQuery('#gacor').insertAfter(jelong);
+		
+		});
